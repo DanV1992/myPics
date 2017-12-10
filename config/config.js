12027e9648dd5e3bda4199aac087684e38d2aba0
DanV1992 <32721355+DanV1992@users.noreply.github.com>
2017-12-09 18:33:06 -0600

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,21 @@
+var path = require('path'),    
+rootPath = path.normalize(__dirname + '/..'),    
+env = process.env.NODE_ENV || 'development';
+
+var config = {  
+    development: {
+        root: rootPath,
+        app: {      name: 'Chirps'    },
+        port: 5000,
+        db: 'mongodb://127.0.0.1/chirps-dev',
+        secret: "cayennedlikedhistreats"
+      },
+    
+ 
+ production: {    
+              root: rootPath,    
+              app: {      name: 'mypics'    },    
+               port: 80,  }
+  };
+
+module.exports = config[env];
